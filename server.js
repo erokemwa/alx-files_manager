359103827d4ca1393e309f4081de0c0730c51c5e
erokemwa <111133254+erokemwa@users.noreply.github.com>
2023-09-05 11:40:42 +0300

    First API

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
+import { env } from 'process';
 import express from 'express';
-import controllerRouting from './routes/index';
+
+const routes = require('./routes/index');
 
 const app = express();
-const port = process.env.PORT || 5000;
-
+const port = env.PORT || 5000;
 app.use(express.json());
-
-controllerRouting(app);
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.use(routes);
+app.listen(port, '127.0.0.1');
 
 export default app;
